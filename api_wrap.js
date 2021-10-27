9108cf55d8a5cf7c35c1ddbab4bcf43f708cd9e8
Leo-Naldi <lnaldi99@gmail.com>
2021-10-27 18:23:27 +0200

    tolte le chiavi

--- api_wrap.js
+++ api_wrap.js
@@ -3,9 +3,9 @@ var process = require('process');
 
 const client_auth = {
 
-    consumer_key: 'fPRqTVDTZ0JfxRIY20Yca1Lzv',
-    consumer_secret: 'UWt9R6Z0o3OEsWgz7cZtQ8MEFOIyGkuUBLgzjxKomEOq0ZhUxi',
-    bearer_token: 'AAAAAAAAAAAAAAAAAAAAAKB5UgEAAAAAQZIJbHa74aQDIGLCy5GazC0GDBk%3D2X8RcQlK3A9hh3wmulT1lcb9n21EYzKEIIdC4xgUTbbCm39cWr',
+    consumer_key: '',
+    consumer_secret: '',
+    bearer_token: '',
     app_only_auth: true,
 };
 
