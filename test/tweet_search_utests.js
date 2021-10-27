a60272ed02307d80bedcfb1c95569d4a87297c29
Leo-Naldi <lnaldi99@gmail.com>
2021-10-27 08:07:20 +0200

    Pending tests per la ricerca dei tweet

---
+++ test/tweet_search_utests.js
@@ -0,0 +1,23 @@
+var assert = require('assert');
+
+describe('Tweet Search Utests', function() {
+
+    describe('#SimpleQuery', function() {
+
+        it('should have a non-empty body');
+
+    });
+
+    describe('#HashtagSearch', function() {
+
+            it('should contain the hashtag');
+
+    });
+
+    describe('#AuthorSearch', function() {
+
+        it('should be by correct author');
+
+    });
+
+});
