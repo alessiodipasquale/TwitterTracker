5737be482ef6bac8167e701fac75ed45bdd837bf
radovic <filip.radovic@studio.unibo.it>
2021-10-31 11:38:17 +0100

    bug fixes for tests

--- test/tweet_search_utests.ts
+++ test/tweet_search_utests.ts
@@ -1,45 +1,34 @@
 import Twitter from "../server/config/Twitter";
-import { expect } from 'chai';
+import {expect} from 'chai';
+
+Twitter.init();
 
 describe('Tweet Search By Keyword utests', () => {
-  const query = { q: "nasa", count: 10 }
-
-  describe('#NonEmptySearch', () => {
-    it('should find at least some tweets', async function() {
-      /* expects to find some tweets */
-      Twitter.searchTweetsByKeyword(query)
-        .then(data => {
-          console.log("testttt");
-          done();
-          expect(data).not.to.be.empty;
-          expect(data.statuses).not.to.be.empty;
-        });
-    });
-  });
-
-  describe('#SimpleQuery', () => {
-    it('should have a non-empty body', async function() {
-      /* expects to find some text */
-      Twitter.searchTweetsByKeyword(query)
-        .then(data => {
-          for (var i = 0; i < data.statuses.length; i++) {
-            var tweet = data.statuses[i];
-            expect(tweet.text).not.to.be.empty;
-          }
-        });
-    });
-  });
-
-  describe.skip('#HashtagSearch', () => {
-
-    it('should contain the hashtag');
-
-  });
-
-  describe.skip('#AuthorSearch', () => {
-
-    it('should be by correct author');
-
-  });
-
+	const query = { q: "nasa", count: 10 }
+	
+	describe('#NonEmptySearch', () => {
+		it('should find at least some tweets', async function() {
+			/* expects to find some tweets */
+			Twitter.searchTweetsByKeyword(query)
+				.then(data => {
+					let res: any = JSON.parse(JSON.stringify(data));
+					expect(res.data).not.to.be.empty;
+					expect(res.data.statuses).not.to.be.empty;
+				});
+		});
+	});
+	
+	describe('#SimpleQuery', () => {
+		it('should have a non-empty body', async function() {
+			/* expects to find some text */
+			Twitter.searchTweetsByKeyword(query)
+				.then(data => {
+					let res: any = JSON.parse(JSON.stringify(data));
+					for (var i = 0; i < res.data.statuses.length; i++) {
+						var tweet = res.data.statuses[i];
+						expect(tweet.text).not.to.be.empty;
+					}
+				});
+		});
+	});
 });
