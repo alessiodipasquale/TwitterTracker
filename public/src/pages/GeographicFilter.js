2cf0f2b82f05673f0b31731a6f581f0be40787c4
Alessio Di Pasquale <alessio.dipasquale@studio.unibo.it>
2021-11-05 19:47:25 +0100

    Added working search on map

--- public/src/pages/GeographicFilter.js
+++ public/src/pages/GeographicFilter.js
@@ -1,62 +1,87 @@
 import React, { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup, useMap, LayerGroup, Circle} from 'react-leaflet'
-import L from "leaflet";
+import L, { LatLng } from "leaflet";
 import icon from "../constants";
 
 import { Col, Row, Container, Form, Button} from "react-bootstrap";
+import { GeoSearchControl, MapBoxProvider } from "leaflet-geosearch";
 
-let center = [49.1951, 16.6068];
-const radius = 3000;
-const fillBlueOptions = { fillColor: 'blue' }
+import { OpenStreetMapProvider } from 'leaflet-geosearch';
+const provider = new OpenStreetMapProvider();
+// add to leaflet
+
 
 function GeographicFilter() {
-  function LocationMarker() {
-    const [position, setPosition] = useState(null);
-    const [bbox, setBbox] = useState([]);
 
-    const map = useMap();
+  let center = [41.8933203,12.4829321];
 
-    useEffect(() => {
-      map.locate().on("locationfound", function (e) {
-        setPosition(e.latlng);
-        map.flyTo(e.latlng, map.getZoom());
-        const circle = L.circle(e.latlng, radius);
-        circle.addTo(map);
-        setBbox(e.bounds.toBBoxString().split(","));
-      });
-    }, [map]);
-    center = position;
-    return position === null ? null : (
-      <Marker position={position} icon={icon}>
-        <Popup>
-          You are here. <br />
-          Map bbox: <br />
-          <b>Southwest lng</b>: {bbox[0]} <br />
-          <b>Southwest lat</b>: {bbox[1]} <br />
-          <b>Northeast lng</b>: {bbox[2]} <br />
-          <b>Northeast lat</b>: {bbox[3]}
-        </Popup>
-      </Marker>
-    );
+  const [data, setData] = useState({
+    city:"",
+    radius: 3000,
+  });
+
+  const [circ,setCirc] = useState({
+    circ: null
+  });
+
+  const [map, setMap] = useState(null);
+
+
+  function handle(e, value) {
+    const newdata = {...data};
+    newdata[e.target.id] = value;
+    setData(newdata);
+}
+
+async function  SearchField  ( ) {
+  const provider = new OpenStreetMapProvider({
+    params: {
+      email: 'john@example.com', // auth for large number of requests
+    },
+  });
+  
+  console.log(data)
+  if(circ.circ != null) {
+    console.log('rimuovo')
+    circ.circ.removeFrom(map);
   }
+
+  const results = await provider.search({ query: data.city });
+
+  const res = results[0];
+  const lat = new LatLng(res.y, res.x);
+
+  center = [res.y, res.x]
+  
+
+  map.flyTo(lat, map.getZoom());
+  
+
+  const circle = L.circle(lat, data.radius);
+  console.log(data.radius)
+  setCirc({circ: circle});
+
+  circle.addTo(map);
+};
+
   return (
     <Container fluid  style={{padding: '2%'}} >
     <Form>
       <Row>
         <Col>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Group className="mb-3" controlId="city">
           <Form.Label>Insert city</Form.Label>
-          <Form.Control type="text" placeholder="Enter city" />
+          <Form.Control onChange={(e)=>handle(e, e.target.value)} type="text" placeholder="Enter city" value={data.city}/>
         </Form.Group>
         </Col>
         <Col>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Group className="mb-3" controlId="radius">
           <Form.Label>Insert radius</Form.Label>
-          <Form.Control type="number" placeholder="Enter Radius" />
+          <Form.Control onChange={(e)=>{handle(e, parseInt(e.target.value))}} type="number" placeholder="Enter Radius" value={data.radius}/>
         </Form.Group>
         </Col>
         <Col style={{display: 'flex', alignItems: 'flex-end', marginBottom: '1rem'}}>
-          <Button variant="primary">Search Tweets</Button>{' '}
+          <Button onClick={() => SearchField()} variant="primary">Search Tweets</Button>{' '}
         </Col>
       </Row>
     </Form>
@@ -65,12 +90,14 @@ function GeographicFilter() {
     zoom={13}
     scrollWheelZoom={false}
     style={{ height: "80vh", width: '50%', borderRadius: '3%'}}
+    whenCreated={setMap}
   >
+
     <TileLayer
       attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
-    <LocationMarker />
+
   </MapContainer>
     
 </Container>
