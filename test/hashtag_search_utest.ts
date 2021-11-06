bce762200ca2249283bef1197ebf8c46c9640371
StefanoColamonaco <stefano.colamonaco@studio.unibo.it>
2021-11-06 12:24:35 +0100

    Typo in previous commit

--- test/hashtag_search_utest.ts
+++ test/hashtag_search_utest.ts
@@ -14,18 +14,18 @@ describe('Tweet Search By Hashtags utests', () => {
       it('should contain the hashtag in the tweet entities', async function() {
         await Twitter.searchTweetsByHashtag(query).then(data => {
 
-          let res: any = JSON.parse(JSON.stringify(data));
-          for (var i = 0; i < res.data.statuses.length; i++) {
-              var tweet = res.data.statuses[i];
-              tweet.text = tweet.text.toUpperCase()
-              for(var j = 0; j<tweet.entities.hashtags.length; j++){
-                tweet.entities.hashtags[j].text = (tweet.entities.hashtags[j].text).toUpperCase();
-              }
-              expect(tweet).to.satisfy(function(tweet: any){
-                if(tweet.text.includes(hashtags[0]) || tweet.entities.hashtags.includes(hashtags[0])) return true;
-                else return false
-              });
-          }
+            let res: any = JSON.parse(JSON.stringify(data));
+            for (var i = 0; i < res.data.statuses.length; i++) {
+                var tweet = res.data.statuses[i];
+                tweet.text = tweet.text.toUpperCase()
+                for(var j = 0; j<tweet.entities.hashtags.length; j++){
+                    tweet.entities.hashtags[j].text = (tweet.entities.hashtags[j].text).toUpperCase();
+                }
+                expect(tweet).to.satisfy(function(tweet: any){
+                    if(tweet.text.includes(hashtags[0]) || tweet.entities.hashtags.includes(hashtags[0])) return true;
+                    else return false
+                });
+            }
         });
       });
     });
