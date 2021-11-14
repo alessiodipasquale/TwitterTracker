5fa36d74462d1deb2185bfc40692c9340e9d03dc
Leo-Naldi <lnaldi99@gmail.com>
2021-11-14 18:20:51 +0100

    added since and until fields in home

--- public/src/pages/Home.js
+++ public/src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { Component, useImperativeHandle, useState } from 'react';
 import Axios from 'axios';
-import { Card, Form, Row, Col, Button, Container } from 'react-bootstrap';
+import { Card, Form, Row, Col, Button, Container, Alert } from 'react-bootstrap';
 
 import TweetCard from '../components/TweetCard';
 import { searchTweet } from '../services/searchTweet-service';
@@ -12,6 +12,9 @@ function Home() {
         author: "",
         remove: "",
     });
+
+    const [since, setSince] = useState(null);
+    const [until, setUntil] = useState(null);
 
     const [tweets,setTweets]=useState([])
 
@@ -62,14 +65,55 @@ function Home() {
           </Row>
 
           <Row>
+          <Col>
+
+
+              <Form.Group class="mb-3" controlId="since">
+
+                <Form.Control
+                onChange={(e)=>handle(e)}
+                placeholder="Only get tweets since..."
+                type="date"
+                selected={data.since}
+                value={data.since}/>
+
+              </Form.Group>
+
+            </Col>
+            <Col>
+
+            <Form.Group class="mb-3" controlId="until">
+
+              <Form.Control
+              onChange={(e)=>handle(e)}
+              type="date"
+              selected={data.until}
+              value={data.until}
+              placeholder="Only get tweets up to..."
+              />
+
+            </Form.Group>
+
+            </Col>
+          </Row>
+
+          <Row>
+            <Alert variant="warning">
+              Because of how the twitter api works, the since and before date will only a week back.
+            </Alert>
+          </Row>
+
+          <Row>
             <Button onClick={(e) => submit(e)} variant="primary">Search Tweets</Button>
           </Row>
         </Form>
+
+
         </Col>
         <Col lg={6}>
 
            <Card style={{ height: '95vh', overflow: 'scroll'}}>
-        
+
           {tweets.length == 0 ?
             <Card.Body  style={{display: "flex", justifyContent: "center", alignItems: "center", textAlign: 'center'}}>
             <div >
@@ -84,12 +128,12 @@ function Home() {
           {
               tweets && tweets.map(tweet=>{
                   return(
-                    <TweetCard tweet={tweet} showOptions={true} />    
+                    <TweetCard tweet={tweet} showOptions={true} />
               )})
           }
          </Card.Body>
           }
-            
+
         </Card>
 
         </Col>
