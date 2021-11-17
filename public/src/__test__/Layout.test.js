20c309238bbcd2d01540f4c46857fe4ad69e4571
radovic <filip.radovic@studio.unibo.it>
2021-11-17 16:44:31 +0100

    Frontend React tests added

---
+++ public/src/__test__/Layout.test.js
@@ -0,0 +1,30 @@
+import { render, screen, cleanup } from '@testing-library/react';
+import { shallow, configure, mount } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import Layout from '../components/Layout';
+configure({adapter: new Adapter()});
+
+describe('Layout tests', () => {
+  console.error = jest.fn(); // disables error log
+
+  test('should render Layout component', () => {
+    render( < Layout / > );
+  });
+
+  describe('should render Navbar', () => {
+    const wrapper = mount( < Layout / > );
+    test('React Pro Sidebar generated', () => {
+      expect(wrapper.find('.pro-sidebar')).toHaveLength(1);
+    });
+    test('Title', () => {
+      expect(wrapper.text().includes('Twitter Tracker')).toBe(true);
+    });
+    test('Home', () => {
+      expect(wrapper.text().includes('Home')).toBe(true);
+    });
+    test('Geographic Filter', () => {
+      expect(wrapper.text().includes('Geographic Filter')).toBe(true);
+    });
+
+  });
+});
