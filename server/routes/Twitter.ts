a8ad768be5805f240f7dda47ae827997bbdac5d9
StefanoColamonaco <stefano.colamonaco@studio.unibo.it>
2021-11-22 23:15:48 +0100

    Installed new package for V2 API

--- server/routes/Twitter.ts
+++ server/routes/Twitter.ts
@@ -1,11 +1,24 @@
 import Twit from "twit";
+import TwitterApi, { TwitterApiReadOnly } from 'twitter-api-v2';
 import Sentiment from "sentiment";
 import Config from "../config/Config";
 
 export default abstract class Twitter {
     private static twit: Twit;
+    private static roClient: TwitterApiReadOnly;
 
     public static async init() {
+        const twitterClient = new TwitterApi({
+            appKey: 'xCjANsVSmJ5hwKKJz6oSZiwOC',
+            appSecret: 'zPzz9otwrXFcMsCDNCubDXG97SNQcCbJEuVeQwa3P5fVlcZV4o',
+            // Following access tokens are not required if you are
+            // at part 1 of user-auth process (ask for a request token)
+            // or if you want a app-only client (see below)
+            accessToken: '1447929992550227969-Xbpzos9Tiu6MUZNY4njk9ZPXCpnncE',
+            accessSecret: 'M2f7dsdiFslNLqRl0FMUv3OpVummKPg2aQhQ4yGfF6XPM',
+        }); 
+        Twitter.roClient = twitterClient.readOnly;
+
         Twitter.twit = new Twit({
             consumer_key: 'xCjANsVSmJ5hwKKJz6oSZiwOC',
             consumer_secret: 'zPzz9otwrXFcMsCDNCubDXG97SNQcCbJEuVeQwa3P5fVlcZV4o',
@@ -15,7 +28,11 @@ export default abstract class Twitter {
     }
 
     public static async searchTweetById(query: any) {
-        return await this.twit.get('statuses/show/:id',query);
+        //return await this.twit.get('statuses/show/:id',query);
+        const data =  await Twitter.roClient.v1.singleTweet(query.id);
+        const result = {data} 
+        return result;
+
     }
 
     public static async searchTweetsByKeyword(query: any) {  // search all tweets that contain keyword
@@ -46,7 +63,7 @@ export default abstract class Twitter {
         const data: any = await this.searchTweetById(query);
         var sentiment = new Sentiment();
         const options: any = Config.sentimentAnalysisOptions;
-        var result = sentiment.analyze(data.data.text, options);
+        var result = sentiment.analyze(data.data.full_text, options);
         return result;
     }
 }
