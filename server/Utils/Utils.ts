cf5aac2c7817fe40028ab26267d2cc8f307e7be5
andrea-corradetti <andreacorradetti28@gmail.com>
2021-11-24 13:24:33 +0100

    refactoring key names and buildQ

--- server/Utils/Utils.ts
+++ server/Utils/Utils.ts
@@ -3,52 +3,30 @@ import qParams from "./Interfaces/QueryParams"
 /* Builds the q field */
 export function buildQ(q: qParams): string {
   
-    var query= "";
+    let query = q.keywords || "";
   
-    if (q.base_query) query = q.base_query.concat(" ");
-  
-    if (q.hashtags){
-      for (var i = 0; i < q.hashtags.length; i++){
-        if (q.hashtags[i].indexOf('#') == -1) {
-          q.hashtags[i] = "#".concat(q.hashtags[i]);
-        }
-      }
+    // if (q.hashtags){
+    //   for (var i = 0; i < q.hashtags.length; i++){
+    //     if (q.hashtags[i].indexOf('#') == -1) {
+    //       q.hashtags[i] = "#".concat(q.hashtags[i]);
+    //     }
+    //   }
+    // }
 
     // q.hashtags && q.hashtags.forEach(element => {
     //   if(element)
     //     query = query.concat("#").concat(element).concat(" ")
     // });
   
-    }
   
-    if (q.author) {
-      query = query.concat(`from:${q.author} `);
-    }
-  
-    if (q.since) {
-      query = query.concat(`since:${q.since} `);
-    }
-  
-    if (q.until) {
-      query = query.concat(`until:${q.until} `);
-    }
+    q.from && (query += " from:" + q.from)
         
-    q.remove && q.remove.forEach(element => {
+    q.exclude && q.exclude.forEach(element => {
       if (element)
         query = query.concat("-").concat(element).concat(" ")
     });
-      
 
-
-    // if (q.remove) {
-    //   for (var i = 0; i < q.remove.length; i++){
-    //     if (q.remove[i].indexOf('-') == -1) {
-    //       q.remove[i] = "-".concat(q.remove[i]);
-    //     }
-    //   }
-  
-    //     query = query.concat(q.remove.join(" ")).concat(" ");
-    // }
+    q.point_radius && (query += " point_radius:" + q.point_radius)
 
     if (q.attitude) {  
       query = query.concat(q.attitude).concat(" ");
