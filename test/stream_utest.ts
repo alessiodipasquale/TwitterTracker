b271d3fe0eb38b4a3b6489ca706bf55800028d3f
radovic <filip.radovic@studio.unibo.it>
2021-11-30 13:15:34 +0100

    Socket tests added

--- test/stream_utest.ts
+++ test/stream_utest.ts
@@ -1,10 +1,56 @@
 import Twitter from "../server/routes/Twitter";
-import { expect } from 'chai';
+import { createServer } from 'http';
+import { expect, assert } from 'chai';
+import { Server } from "socket.io";
+import { io as Client } from "socket.io-client";
 import { delay } from "../server/Utils/Utils"
+import { setListenersForSocket } from "../server/routes/StreamManager"
 
 Twitter.authentication();
 
 describe('Stream utests', () => {
+
+  describe('#SocketTests', () => {
+    let io: any, serverSocket: any, clientSocket: any;
+
+    before((done) => {
+      const httpServer: any = createServer();
+      io = new Server(httpServer);
+      httpServer.listen(() => {
+        const port = httpServer.address().port;
+        clientSocket = new (Client as any)(`http://localhost:${port}`);
+        io.on("connection", (socket: any) => {
+          serverSocket = socket;
+        });
+        clientSocket.on("connect", done);
+      });
+    });
+
+    after(() => {
+      io.close();
+      clientSocket.close();
+    });
+
+    it("should work when testing socket connection", (done) => {
+      clientSocket.once("ao", (arg: any) => {
+        assert.equal(arg.test, "ao");
+        done();
+      })
+      setListenersForSocket(serverSocket);
+      clientSocket.emit("/testSocketConnection", "test");
+    });
+
+    it("should receive data", (done) => {
+      clientSocket.once("dataFromLiteraryContests", (arg: any) => {
+        const res: any = JSON.parse(JSON.stringify(arg)).dataFromLiteraryContests;
+        assert.equal(res[0].name, "#streamTestswe4");
+        done();
+      })
+      setListenersForSocket(serverSocket);
+      clientSocket.emit("/readyToReceiveData", "test");
+    });
+
+  });
 
   describe('#StreamDefinitions', () => {
     it('should start and close streaming', async function() {
@@ -14,7 +60,6 @@ describe('Stream utests', () => {
       })();
       await delay(1000);
       await Twitter.startStream();
-      await delay(5000);
       await Twitter.stream.close();
       console.log = oldLog;
     });
@@ -23,21 +68,21 @@ describe('Stream utests', () => {
       var oldLog = console.log;
       (function() {
         console.log = function(msg: string) {
-          if(typeof msg[0] == 'undefined'){
+          if (typeof msg[0] == 'undefined') {
             const res = JSON.parse(JSON.stringify(msg));
             const date: Date = new Date(res.meta.sent);
             const today: Date = new Date();
-            const yesterday : Date = new Date(today.getTime() - (24 * 60 * 60 * 1000));
-            const tomorrow : Date = new Date(today.getTime() + (24 * 60 * 60 * 1000));
-            expect(today.getDate()).to.be.within(yesterday.getDate(), tomorrow.getDate());
+            const yesterday: Date = new Date(today.getTime() - (24 * 60 * 60 * 1000));
+            const tomorrow: Date = new Date(today.getTime() + (24 * 60 * 60 * 1000));
+            expect(today.getTime()).to.be.within(yesterday.getTime(), tomorrow.getTime());
           }
         };
       })();
       await delay(1000);
       await Twitter.startStream();
-      await delay(5000);
       await Twitter.stream.close();
       console.log = oldLog;
     });
   });
+
 });
