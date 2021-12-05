e1371fdee614e4f1fa0f584edfaf6582d90ac0ab
radovic <filip.radovic@studio.unibo.it>
2021-12-06 00:24:44 +0100

    Trivia managment unit test dummy v2

--- test/trivia_management_utest.ts
+++ test/trivia_management_utest.ts
@@ -1,4 +1,4 @@
-/*import Twitter from "../server/routes/Twitter";
+import Twitter from "../server/routes/Twitter";
 import fetch from 'node-fetch';
 import { createServer, Server} from 'http';
 import { expect, assert } from 'chai';
@@ -6,71 +6,103 @@ import { Server as SocketServer } from "socket.io";
 import { io as Client } from "socket.io-client";
 import { delay } from "../server/Utils/Utils"
 import { setListenersForSocket } from "../server/routes/StreamManager"
+// import {execaNode} from 'execa'; // da i problemi
+const axios = require('axios').default;
 
 Twitter.authentication();
 
-describe('Trivia manage utests', () => {
+describe.skip('Trivia manage utests', () => {
 
-  describe('#Game creation Tests', () => {
-    let io: any, serverSocket: any, clientSocket: any, httpServer: Server;
+	describe('#Game creation Tests', () => {
+		let io: any, serverSocket: any, clientSocket: any, httpServer: Server;
+		
+		before((done) => {
+			/*
+			qui si fa il fork e si crea il pipe dal stdout di figlio al stdout del processo padre
+			dopo di questo si possono fare le richieste a localhost:8000
+			*/
+			/*
+			const trivia_host = execaNode('../out/index').stdout.pipe(process.stdout);;
+			*/
 
-    before((done) => {
-      httpServer = createServer();
-      io = new SocketServer(httpServer);
-      httpServer.listen(() => {
-        const addressInfo = httpServer.address() as any;
-        const port = addressInfo.port;
-        clientSocket = new (Client as any)(`http://localhost:${port}`);
-        io.on("connection", (socket: any) => {
-          serverSocket = socket;
-        });
-        clientSocket.on("connect", done);
-      });
-    });
+			/*
+			--- CODICE VECCHIO ---
+			httpServer = createServer();
+			io = new SocketServer(httpServer);
+			httpServer.listen(() => {
+				const addressInfo = httpServer.address() as any;
+				const port = addressInfo.port;
+				clientSocket = new (Client as any)(`http://localhost:${port}`);
+				io.on("connection", (socket: any) => {
+					serverSocket = socket;
+				});
+				clientSocket.on("connect", done);
+			});
+			*/
+		});
 
-    after(() => {
-      io.close();
-      clientSocket.close();
-    });
+		after(() => {
+			/*
+			  trivia_host.kill();
+			*/
+			
+			/*
+			--- CODICE VECCHIO ---
+			io.close();
+			clientSocket.close();
+			*/
+		});
+		
+		it("Should create a new game to test", (done) => {
+			const streamDefinitions = {
+				"name":"#swe_4_test_triviaGames",
+				"type": "triviaGame",
+				"startDate": "2022-11-28T00:00:00.000Z",
+				"endDate": "2022-11-28T00:00:00.000Z",
+				"rules":[
+					{
+						"value": "#swe_4_test_triviaGames risposta_1:",
+						"tag": "risposta_1"
+					},
+					{
+						"value": "#swe_4_test_triviaGames risposta_2:",
+						"tag": "risposta_2"
+					}
+				],
+				"extras":{
+					"questions":[
+						{
+							"number":1,
+							"text": "Text question 1",
+							"correctAnswers":["correctAnswer1"],
+						},
+						{
+							"number":2,
+							"text": "Text question 2",
+							"correctAnswers":["correctAnswer2"],
+						}
+					]
+				}
+			}
+			/*
+			  fa le richiesta al localhost:8000
+			*/
+			/*
+			  const res1 = await axios.post('http://localhost:8000/addElementToStreamData', {streamDefinitions: streamDefinitions});
+			  const res2 = await axios.delete(('http://localhost:8000/removeStreamElementFromData/' + streamDefinitions.name));
+			*/
 
-    it("Should create a new game to test", async (done) => {
-        const addressInfo = httpServer.address() as any;
-        const port = addressInfo.port;
-        const streamDefinitions = {
-            "name":"#swe_4_test_triviaGames",
-            "type": "triviaGame",
-            "startDate": "2022-11-28T00:00:00.000Z",
-            "endDate": "2022-11-28T00:00:00.000Z",
-            "rules":[
-                {
-                    "value": "#swe_4_test_triviaGames risposta_1:",
-                    "tag": "risposta_1"
-                },
-                {
-                    "value": "#swe_4_test_triviaGames risposta_2:",
-                    "tag": "risposta_2"
-                }
-            ],
-            "extras":{
-                "questions":[
-                    {
-                        "number":1,
-                        "text": "Text question 1",
-                        "correctAnswers":["correctAnswer1"],
-                    },
-                    {
-                        "number":2,
-                        "text": "Text question 2",
-                        "correctAnswers":["correctAnswer2"],
-                    }
-                ]
-            }
-        }
-        const stringedBody = JSON.stringify(streamDefinitions)
-        const response = await fetch(`http://localhost:${port}/addElementToStreamData`, {method: 'POST', body: stringedBody});
-        const response = await fetch(`http://localhost:${port}/addElementToStreamData`, {method: 'DELETE'});
-    });
-  });
+			/*
+			--- CODICE VECCHIO ---
+			const addressInfo = httpServer.address() as any;
+			const port = addressInfo.port;
+			
+			const stringedBody = JSON.stringify(streamDefinitions);			
+			const res1 = await axios.post(`http://localhost:8000/addElementToStreamData`, {streamDefinitions: streamDefinitions});
+			console.log(res1);
+			// const res2 = await fetch(`http://localhost:${port}/removeStreamElementFromData`, {method: 'DELETE'});
+			*/
+			done();
+		});
+	});
 });
-
-*/
