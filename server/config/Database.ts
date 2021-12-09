8f39f0ab8791f06eba97066d38727e1535ceb598
StefanoColamonaco <stefano.colamonaco@studio.unibo.it>
2021-12-09 19:41:37 +0100

    Implemented possibility of vote a book that is not candidated

--- server/config/Database.ts
+++ server/config/Database.ts
@@ -12,7 +12,7 @@ export default abstract class Database {
 
     public static get streamDefinitions(): StreamDefinition[] {
         let objectArray = JSON.parse(JSON.stringify(Data.streamDefinitions));
-        for(let elem of objectArray){
+        for (let elem of objectArray) {
             elem.startDate = new Date(elem.startDate);
             elem.startDate = new Date(elem.endDate)
         }
@@ -21,8 +21,8 @@ export default abstract class Database {
 
     public static set streamDefinitions(definitions: StreamDefinition[]) {
         let allData = Data;
-        const objectData = JSON.parse(JSON.stringify({streamDefinitions: definitions}));
-        for(let elem of objectData){
+        const objectData = JSON.parse(JSON.stringify({ streamDefinitions: definitions }));
+        for (let elem of objectData) {
             elem.startDate = new Date(elem.startDate).toISOString().substring(0, 10);
             elem.endDate = new Date(elem.endDate).toISOString().substring(0, 10);
         }
@@ -35,68 +35,68 @@ export default abstract class Database {
         }
     }
 
-    public static newStreamDef(newStream: StreamDefinition){
+    public static newStreamDef(newStream: StreamDefinition) {
         let currentStreamDefs: StreamDefinition[] = (Database.streamDefinitions) as StreamDefinition[];
         currentStreamDefs = currentStreamDefs.concat(newStream);
-        switch(newStream.type){
-            case 'literaryContest':{
+        switch (newStream.type) {
+            case 'literaryContest': {
                 const done = Database.newLiteraryContest(newStream);
-                if(done)
-                    Socket.broadcast('newLiteraryContestCreated',newStream)
+                if (done)
+                    Socket.broadcast('newLiteraryContestCreated', newStream)
                 break;
             }
-            case 'triviaGame':{
+            case 'triviaGame': {
                 const done = Database.newTriviaGame(newStream);
-                if(done)
-                    Socket.broadcast('newTriviaGameCreated',newStream)
+                if (done)
+                    Socket.broadcast('newTriviaGameCreated', newStream)
                 break;
             }
-            default:{
+            default: {
                 console.log('Unrecognized type of stream');
             }
         }
         Database.streamDefinitions = currentStreamDefs;
     }
 
-    public static deleteStreamDef(toDelete: string, type:string){
+    public static deleteStreamDef(toDelete: string, type: string) {
         let currentStreamDefs: StreamDefinition[] = (Database.streamDefinitions) as StreamDefinition[];
-        currentStreamDefs = currentStreamDefs.filter((element)=>{
+        currentStreamDefs = currentStreamDefs.filter((element) => {
             return element.name != toDelete
         })
         Database.streamDefinitions = currentStreamDefs;
-        if(true){
+        if (true) {
             Database.deleteStreamData(toDelete, type)
         }
     }
 
-    public static deleteStreamData(toDelete: string, type:string){
-        let currentStreamData: any[] = []; 
-        if(type == 'triviaGame')
+    public static deleteStreamData(toDelete: string, type: string) {
+        let currentStreamData: any[] = [];
+        if (type == 'triviaGame')
             currentStreamData = Database.triviaGamesData
-        if(type == 'literaryContest')
+        if (type == 'literaryContest')
             currentStreamData = Database.literaryContestsData
-        
-        currentStreamData = currentStreamData.filter((element)=>{
+
+        currentStreamData = currentStreamData.filter((element) => {
             return element.name != toDelete
         })
 
-        if(type == 'triviaGame')
+        if (type == 'triviaGame')
             Database.triviaGamesData = currentStreamData;
-        if(type == 'literaryContest')
+        if (type == 'literaryContest')
             Database.literaryContestsData = currentStreamData;
     }
 
-    public static get literaryContestsData(){
+    public static get literaryContestsData() {
         let objectArray = JSON.parse(JSON.stringify(Data.DataFromLiteraryContests));
         return objectArray;
     }
 
-    public static get triviaGamesData(){
+    public static get triviaGamesData() {
         let objectArray = JSON.parse(JSON.stringify(Data.DataFromTriviaGames));
         return objectArray;
     }
 
-    public static set literaryContestsData(newData: any[]){
+    public static set literaryContestsData(newData: any[]) {
         let allData = Data;
         allData.DataFromLiteraryContests = newData;
         const stringedData = JSON.stringify(allData)
@@ -107,7 +107,7 @@ export default abstract class Database {
         }
     }
 
-    public static set triviaGamesData(newData: any[]){
+    public static set triviaGamesData(newData: any[]) {
         let allData = Data;
         allData.DataFromTriviaGames = newData
         const stringedData = JSON.stringify(allData)
@@ -118,11 +118,11 @@ export default abstract class Database {
         }
     }
 
-    public static newLiteraryContest(newStream: StreamDefinition){
-        if(!Database.eventAlreadyPresent(newStream.name,"literaryContest")){
+    public static newLiteraryContest(newStream: StreamDefinition) {
+        if (!Database.eventAlreadyPresent(newStream.name, "literaryContest")) {
             let allData = Data;
             const objectData = allData.DataFromLiteraryContests;
-            objectData.push({name: newStream.name, voters: [], books:[]});
+            objectData.push({ name: newStream.name, voters: [], books: [] });
             allData.DataFromLiteraryContests = objectData;
             const stringedData = JSON.stringify(allData)
             try {
@@ -135,20 +135,20 @@ export default abstract class Database {
         return false;
     }
 
-    public static newTriviaGame(newStream: StreamDefinition){
-        if(!Database.eventAlreadyPresent(newStream.name,"triviaGame")){
+    public static newTriviaGame(newStream: StreamDefinition) {
+        if (!Database.eventAlreadyPresent(newStream.name, "triviaGame")) {
             let allData = Data;
             const objectData = allData.DataFromTriviaGames;
             const buildedQuestions = [];
-            for(let question of newStream.extras.questions){
+            for (let question of newStream.extras.questions) {
                 let lowerCase = []
-                for(let cAnswer of question.correctAnswers){
+                for (let cAnswer of question.correctAnswers) {
                     lowerCase.push(cAnswer.toLowerCase())
                 }
-                const obj = {number:question.number, text:question.text, correctAnswers:lowerCase, participants:[]}
+                const obj = { number: question.number, text: question.text, correctAnswers: lowerCase, participants: [] }
                 buildedQuestions.push(obj)
             }
-            objectData.push({name: newStream.name, questions: buildedQuestions});
+            objectData.push({ name: newStream.name, questions: buildedQuestions });
             allData.DataFromTriviaGames = objectData;
             const stringedData = JSON.stringify(allData)
             try {
@@ -161,35 +161,35 @@ export default abstract class Database {
         return false;
     }
 
-    private static eventAlreadyPresent(name: string, type:string){
+    private static eventAlreadyPresent(name: string, type: string) {
         let data;
-        if(type=='literaryContest')
+        if (type == 'literaryContest')
             data = Data.DataFromLiteraryContests;
         else data = Data.DataFromTriviaGames
-        for(let elem of data){
-            if(elem.name == name)
+        for (let elem of data) {
+            if (elem.name == name)
                 return true;
         }
         return false;
     }
 
-    public static getTypeFromHashtag(hashtag: string){
+    public static getTypeFromHashtag(hashtag: string) {
         const streamDefinitions = Database.streamDefinitions;
-        for(let element of streamDefinitions){
-            if(element.name == hashtag){
+        for (let element of streamDefinitions) {
+            if (element.name == hashtag) {
                 return element.type;
             }
         }
         return -1;
     }
 
-    public static candidateNewBook(hashtag:string, name:string, author_id:string){
-        if(!Database.bookAlreadyPresent(hashtag,name)){
+    public static candidateNewBook(hashtag: string, name: string, author_id: string) {
+        if (!Database.bookAlreadyPresent(hashtag, name)) {
             let allData = Data;
             const objectData = allData.DataFromLiteraryContests;
-            for(let elem of objectData){
-                if(elem.name == hashtag){
-                    elem.books.push({candidatedBy:author_id,bookName:name, votes:0, votedBy:[]})
+            for (let elem of objectData) {
+                if (elem.name == hashtag) {
+                    elem.books.push({ candidatedBy: author_id, bookName: name, votes: 0, votedBy: [] })
                 }
             }
             allData.DataFromLiteraryContests = objectData;
@@ -203,99 +203,104 @@ export default abstract class Database {
         }
         return false;
     }
-    private static bookAlreadyPresent(hashtag:string, name:string){
+    private static bookAlreadyPresent(hashtag: string, name: string) {
         const data = Data.DataFromLiteraryContests;
-        for(let contest of data){
-            if(contest.name == hashtag){
-                for(let book of contest.books){
-                    if(book.bookName.toLowerCase() == name.toLowerCase()){
+        for (let contest of data) {
+            if (contest.name == hashtag) {
+                for (let book of contest.books) {
+                    if (book.bookName.toLowerCase() == name.toLowerCase()) {
                         return true;
                     }
                 }
-            } 
+            }
         }
         return false;
     }
 
-    public static voteBook(hashtag:string, name:string, author_id:string){
-        if(!Database.reachedMaxVotes(hashtag,author_id) && !Database.hasVotedBook(hashtag,name,author_id) && Database.bookAlreadyPresent(hashtag,name)){
-            let allData = Data;
-            const objectData = allData.DataFromLiteraryContests;
-            for(let contest of objectData){
-                if(contest.name == hashtag){
-                    //incrementare voto in voters
-                    let found = false;
-                    for(let voter of contest.voters){
-                        if(voter.author_id == author_id){
-                            found = true;
-                            voter.numVotes = voter.numVotes+1;
+    public static voteBook(hashtag: string, name: string, author_id: string) {
+        if (Database.bookAlreadyPresent(hashtag, name)) {
+            if (!Database.reachedMaxVotes(hashtag, author_id) && !Database.hasVotedBook(hashtag, name, author_id)){
+                let allData = Data;
+                const objectData = allData.DataFromLiteraryContests;
+                for (let contest of objectData) {
+                    if (contest.name == hashtag) {
+                        //incrementare voto in voters
+                        let found = false;
+                        for (let voter of contest.voters) {
+                            if (voter.author_id == author_id) {
+                                found = true;
+                                voter.numVotes = voter.numVotes + 1;
+                            }
                         }
-                    }
-                    if(!found){
-                        contest.voters.push({author_id, numVotes:1})
-                    }
-                    //aggiungere ai votanti e ai voti del libro
-                    for(let book of contest.books){
-                        if(book.bookName.toLowerCase() == name.toLowerCase()){
-                            book.votes = book.votes+1;
-                            book.votedBy.push(author_id)
+                        if (!found) {
+                            contest.voters.push({ author_id, numVotes: 1 })
+                        }
+                        //aggiungere ai votanti e ai voti del libro
+                        for (let book of contest.books) {
+                            if (book.bookName.toLowerCase() == name.toLowerCase()) {
+                                book.votes = book.votes + 1;
+                                book.votedBy.push(author_id)
+                            }
                         }
                     }
                 }
+                allData.DataFromLiteraryContests = objectData;
+                const stringedData = JSON.stringify(allData)
+                try {
+                    fs.writeFileSync('./server/config/Data.json', stringedData);
+                } catch (error) {
+                    console.error(error);
+                }
+                return true;
             }
-            allData.DataFromLiteraryContests = objectData;
-            const stringedData = JSON.stringify(allData)
-            try {
-                fs.writeFileSync('./server/config/Data.json', stringedData);
-            } catch (error) {
-                console.error(error);
-            }
-            return true;
+        } else {
+            Database.candidateNewBook(hashtag,name,author_id)
+            Database.voteBook(hashtag,name,author_id)
         }
         return false;
     }
 
-    private static reachedMaxVotes(hashtag:string ,author_id: string){
+    private static reachedMaxVotes(hashtag: string, author_id: string) {
         const data = Data.DataFromLiteraryContests;
-        for(let contest of data){
-            if(contest.name == hashtag) {
-                for(let voter of contest.voters){
-                    if(voter.author_id == author_id && voter.numVotes == Config.maxVotes)
+        for (let contest of data) {
+            if (contest.name == hashtag) {
+                for (let voter of contest.voters) {
+                    if (voter.author_id == author_id && voter.numVotes == Config.maxVotes)
                         return true;
                 }
-            } 
+            }
         }
         return false;
     }
 
-    private static hasVotedBook(hashtag:string ,name:string, author_id: string){
+    private static hasVotedBook(hashtag: string, name: string, author_id: string) {
         const data = Data.DataFromLiteraryContests;
-        for(let contest of data){
-            if(contest.name == hashtag) {
-                for(let book of contest.books){
-                    if(book.bookName.toLowerCase() == name.toLowerCase()){
-                        for(let voter of book.votedBy){
-                            if(voter == author_id)
+        for (let contest of data) {
+            if (contest.name == hashtag) {
+                for (let book of contest.books) {
+                    if (book.bookName.toLowerCase() == name.toLowerCase()) {
+                        for (let voter of book.votedBy) {
+                            if (voter == author_id)
                                 return true;
                         }
                     }
                 }
-            } 
+            }
         }
         return false;
     }
 
-    public static registerAnswer(hashtag:string,answerNumber:number, answer:string, author_id:string, username:string){
-        if(!Database.hasAlreadyAnswered(hashtag,answerNumber, author_id)){
+    public static registerAnswer(hashtag: string, answerNumber: number, answer: string, author_id: string, username: string) {
+        if (!Database.hasAlreadyAnswered(hashtag, answerNumber, author_id)) {
             let allData = Data;
             const objectData = allData.DataFromTriviaGames;
             let correct;
-            for(let game of objectData){
-                if(game.name == hashtag){
-                    for(let question of game.questions){
-                        if(question.number == answerNumber){
+            for (let game of objectData) {
+                if (game.name == hashtag) {
+                    for (let question of game.questions) {
+                        if (question.number == answerNumber) {
                             correct = question.correctAnswers.includes(answer.toLowerCase())
-                            question.participants.push({username: username, userId:author_id, answeredTo: answerNumber, answer:answer, isCorrect:correct})
+                            question.participants.push({ username: username, userId: author_id, answeredTo: answerNumber, answer: answer, isCorrect: correct })
                         }
                     }
                 }
@@ -312,19 +317,19 @@ export default abstract class Database {
         return -1;
     }
 
-    private static hasAlreadyAnswered(hashtag:string, answerNumber:number, author_id: string){
+    private static hasAlreadyAnswered(hashtag: string, answerNumber: number, author_id: string) {
         const data = Data.DataFromTriviaGames;
-        for(let game of data){
-            if(game.name == hashtag) {
-                for(let question of game.questions){
-                    if(question.number == answerNumber){
-                        for(let participant of question.participants){
-                            if(participant.userId == author_id)
+        for (let game of data) {
+            if (game.name == hashtag) {
+                for (let question of game.questions) {
+                    if (question.number == answerNumber) {
+                        for (let participant of question.participants) {
+                            if (participant.userId == author_id)
                                 return true;
                         }
                     }
                 }
-            } 
+            }
         }
         return false;
     }
