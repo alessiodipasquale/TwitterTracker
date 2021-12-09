f203ebc6389387b27a6ff158a0064f88824c20a2
StefanoColamonaco <stefano.colamonaco@studio.unibo.it>
2021-12-09 23:18:06 +0100

    Added paginator management for big quantities of tweets

--- server/routes/RoutesManager.ts
+++ server/routes/RoutesManager.ts
@@ -2,128 +2,161 @@ import { BadRequest } from '../config/Error';
 import { Stopwatch } from 'ts-stopwatch';
 import { IRequest, IResponse } from '../config/Express';
 import Twitter from "./Twitter";
-import { buildQ, formatData, translateAndGetSentiments } from "../Utils/Utils";
-import { Tweetv2SearchParams } from 'twitter-api-v2';
+import { buildQ, delay, formatData, translateAndGetSentiments } from "../Utils/Utils";
+import { Tweetv2SearchParams, TweetSearchAllV2Paginator } from 'twitter-api-v2';
 import Database from '../config/Database';
 import { StreamDefinition, Rule } from '../types/StreamDefinition'
 import Config from '../config/Config';
 import Socket from '../connection/Socket';
 
-export const searchTweetById: any = async(req: IRequest, res:IResponse) : Promise<void> => {
+export const searchTweetById: any = async (req: IRequest, res: IResponse): Promise<void> => {
     const id: string = req.params.tweetId;
-    Twitter.searchTweetById(id,{})
-    .then(data => {
-        res.send(data)
-    }).catch(err => {
-        throw new BadRequest('INCORRECT_BODY', `Il body non è corretto`)
-    })
+    Twitter.searchTweetById(id, {})
+        .then(data => {
+            res.send(data)
+        }).catch(err => {
+            throw new BadRequest('INCORRECT_BODY', `Il body non è corretto`)
+        })
 }
 
-export const searchByKeyword: any = async(req: IRequest, res:IResponse) : Promise<void> => {
-    const optionalParams : Partial<Tweetv2SearchParams> = {
+export const searchByKeyword: any = async (req: IRequest, res: IResponse): Promise<void> => {
+    const optionalParams: Partial<Tweetv2SearchParams> = {
         start_time: req.body.since,
         end_time: req.body.until,
         max_results: req.body.count ?? 15,
         expansions: Config.standardSearchOptions.expansions,
-        'place.fields':Config.standardSearchOptions['place.fields'],
-        'tweet.fields':Config.standardSearchOptions['tweet.fields'],
+        'place.fields': Config.standardSearchOptions['place.fields'],
+        'tweet.fields': Config.standardSearchOptions['tweet.fields'],
         'user.fields': Config.standardSearchOptions['user.fields']
     }
-    
+
+    const controlNumber: number = optionalParams.max_results! as number;
+    if(controlNumber >100) optionalParams.max_results = 100;
+
     //'[lat lon raggioinkm]'
-    const queryParams = { 
+    const queryParams = {
         keywords: req.body.text ?? "",
         point_radius: req.body.geocode,
         from: req.body.author ?? "",
         exclude: (req.body.remove).split(" ") ?? "",
         attitude: req.body.attitude ?? "",
     }
-    
-    let queryOptions : Partial<Tweetv2SearchParams> | undefined = Object.fromEntries(Object.entries(optionalParams).filter(([_, v]) => v != null && v !==""));
+
+    let queryOptions: Partial<Tweetv2SearchParams> | undefined = Object.fromEntries(Object.entries(optionalParams).filter(([_, v]) => v != null && v !== ""));
     let queryPath: string = buildQ(queryParams)
 
     const stopwatch = new Stopwatch()
     stopwatch.start();
 
-    Twitter.searchTweetsByKeyword({query: queryPath, options: queryOptions})
-    .then(paginator => {
-        let formattedData:any = {}; 
-        formattedData.tweets = formatData(paginator.data);
-        formattedData.dataRetrievingTime = {time:stopwatch.getTime(), result_count:paginator.meta.result_count}
-        res.send(formattedData)
-    })
-    .catch(err => {
-        console.log(err.stack)
-        res.status(400).send({ error: 'INCORRECT_BODY', description: `Il body non è corretto` });
-        //throw new BadRequest('INCORRECT_BODY', `Il body non è corretto`)
-    })
+    Twitter.searchTweetsByKeyword({ query: queryPath, options: queryOptions })
+        .then(async (paginator: TweetSearchAllV2Paginator) => {
+
+            if (controlNumber > 100) {
+                const paginators = [];
+                let tot = paginator.meta.result_count;
+                let formattedData: any = {};
+                formattedData.tweets = [];
+
+                paginators.push(paginator);
+                while (paginator.meta.next_token && tot<controlNumber) {
+                    paginator = await paginator.fetchNext();
+                    tot = paginator.meta.result_count;
+                    paginators.push(paginator);
+                    delay(500)
+                }
+                let count = 0;
+                for (let elem of paginators) {
+                    const data = formatData(elem.data);
+                    for (let tweet of data) {
+                        if(count <= controlNumber){
+                            formattedData.tweets.push(tweet)
+                            count = count+1;
+                        }
+                    }
+                }
+                if(tot > controlNumber) tot = controlNumber;
+                formattedData.dataRetrievingTime = { time: stopwatch.getTime(), result_count: tot }
+                res.send(formattedData)
+            } else {
+                let formattedData: any = {};
+                formattedData.tweets = formatData(paginator.data);
+                formattedData.dataRetrievingTime = { time: stopwatch.getTime(), result_count: paginator.meta.result_count }
+                res.send(formattedData)
+            }
+
+        })
+        .catch(err => {
+            console.log(err.stack)
+            res.status(400).send({ error: 'INCORRECT_BODY', description: `Il body non è corretto` });
+            //throw new BadRequest('INCORRECT_BODY', `Il body non è corretto`)
+        })
 }
 
-export const getUserInformations: any = async(req: IRequest, res:IResponse) : Promise<void> => {
+export const getUserInformations: any = async (req: IRequest, res: IResponse): Promise<void> => {
     const options = {}
     var query;
-    if(req.body.id){
-        query = { id: req.body.id}
-    }else{
+    if (req.body.id) {
+        query = { id: req.body.id }
+    } else {
         query = { username: req.body.username }
     }
-    Twitter.getUserInformations({query,options})
-    .then(data => {
-        res.send(data)
-    }).catch(err => {
-        throw new BadRequest('INCORRECT_BODY', `Il body non è corretto`)
-    })
-}
-export const getRetweetsByTweetId: any = async(req: IRequest, res:IResponse) : Promise<void> => {
-    const options = {}
-    const id: string = req.params.tweetId;
-    const query = {id:id};
-    console.log(query);
-    Twitter.getRetweetsByTweetId({query, options})
-    .then(data => {
-        res.send(data)
-    }).catch(err => {
-        throw new BadRequest('INCORRECT_BODY', `Il body non è corretto`)
-    })
-}
-export const getRetweetersByTweetId: any = async(req: IRequest, res:IResponse) : Promise<void> => {
-    const options = {}
-    const id: string = req.params.tweetId;
-    const query = {id:id};
-
-    Twitter.getRetweetersByTweetId({query, options})
-    .then(data => {
-        res.send(data)
-    }).catch(err => {
-        throw new BadRequest('INCORRECT_BODY', `Il body non è corretto`)
-    })
-}
-export const getSentimentFromTweet: any = async(req: IRequest, res:IResponse) : Promise<void> => {
-    const id: string = req.params.tweetId;
-
-    Twitter.searchTweetById(id, {})
-    .then(async (data) => {
-        const result = await translateAndGetSentiments(data.data.text);
-        res.send(result);
-    }).catch(err => {
-        throw new BadRequest('INCORRECT_BODY', `Il body non è corretto`)
-    })
-}
-
-export const getSentimentFromGroupOfTweets: any = async(req: IRequest, res:IResponse) : Promise<void> => {
-    let ids: string[] = [];
-    let toAnalize: string = ""
-    let tweets = req.data.data;
-    for(let t of tweets){
-        ids.push(t.id);
-    }
-    for(let id of ids){
-        Twitter.searchTweetById(id, {})
+    Twitter.getUserInformations({ query, options })
         .then(data => {
-            toAnalize.concat(data.data.text);
+            res.send(data)
         }).catch(err => {
             throw new BadRequest('INCORRECT_BODY', `Il body non è corretto`)
         })
+}
+export const getRetweetsByTweetId: any = async (req: IRequest, res: IResponse): Promise<void> => {
+    const options = {}
+    const id: string = req.params.tweetId;
+    const query = { id: id };
+    console.log(query);
+    Twitter.getRetweetsByTweetId({ query, options })
+        .then(data => {
+            res.send(data)
+        }).catch(err => {
+            throw new BadRequest('INCORRECT_BODY', `Il body non è corretto`)
+        })
+}
+export const getRetweetersByTweetId: any = async (req: IRequest, res: IResponse): Promise<void> => {
+    const options = {}
+    const id: string = req.params.tweetId;
+    const query = { id: id };
+
+    Twitter.getRetweetersByTweetId({ query, options })
+        .then(data => {
+            res.send(data)
+        }).catch(err => {
+            throw new BadRequest('INCORRECT_BODY', `Il body non è corretto`)
+        })
+}
+export const getSentimentFromTweet: any = async (req: IRequest, res: IResponse): Promise<void> => {
+    const id: string = req.params.tweetId;
+
+    Twitter.searchTweetById(id, {})
+        .then(async (data) => {
+            const result = await translateAndGetSentiments(data.data.text);
+            res.send(result);
+        }).catch(err => {
+            throw new BadRequest('INCORRECT_BODY', `Il body non è corretto`)
+        })
+}
+
+export const getSentimentFromGroupOfTweets: any = async (req: IRequest, res: IResponse): Promise<void> => {
+    let ids: string[] = [];
+    let toAnalize: string = ""
+    let tweets = req.data.data;
+    for (let t of tweets) {
+        ids.push(t.id);
+    }
+    for (let id of ids) {
+        Twitter.searchTweetById(id, {})
+            .then(data => {
+                toAnalize.concat(data.data.text);
+            }).catch(err => {
+                throw new BadRequest('INCORRECT_BODY', `Il body non è corretto`)
+            })
     }
     let toReturn: any = {};
     const result = await translateAndGetSentiments(toAnalize);
@@ -132,22 +165,22 @@ export const getSentimentFromGroupOfTweets: any = async(req: IRequest, res:IResp
     res.send(toReturn);
 }
 
-export const addElementToStreamData = async (req: IRequest, res:IResponse) => {
-    try{
+export const addElementToStreamData = async (req: IRequest, res: IResponse) => {
+    try {
         Database.newStreamDef(req.body.streamDefinitions as StreamDefinition);
         Twitter.rulesConstruction(req.body.streamDefinitions, "add");
         res.send();
-    }catch(err){
+    } catch (err) {
         throw new BadRequest('INCORRECT_BODY', `Il body non è corretto`)
     }
 }
 
-export const removeStreamElementFromData = async (req: IRequest, res:IResponse) => {
-    try{
+export const removeStreamElementFromData = async (req: IRequest, res: IResponse) => {
+    try {
         await Twitter.removeFromRules(req.params.streamName);
         Database.deleteStreamDef(req.params.streamName, req.params.type)
         res.send();
-    }catch(err){
+    } catch (err) {
         throw new BadRequest('INCORRECT_BODY', `Il body non è corretto`)
     }
 }
