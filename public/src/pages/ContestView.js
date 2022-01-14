af90fb9bf77c72e9a083346e40ed1d9d1478f1ed
StefanoColamonaco <stefano.colamonaco@studio.unibo.it>
2022-01-14 16:44:20 +0100

    New implementation for clipboard that supports http

--- public/src/pages/ContestView.js
+++ public/src/pages/ContestView.js
@@ -520,8 +520,23 @@ class ContestView extends Component {
                               text = text+res;
                               num = num +1;
                             }
+                            /*
                             navigator.clipboard.writeText(text)
-                            alert("Data copied to clipboard");
+                            */let textArea = document.createElement("textarea");
+                              textArea.value = text;
+                              // make the textarea out of viewport
+                              textArea.style.position = "fixed";
+                              textArea.style.left = "-999999px";
+                              textArea.style.top = "-999999px";
+                              document.body.appendChild(textArea);
+                              textArea.focus();
+                              textArea.select();
+                              return new Promise((res, rej) => {
+                                  // here the magic happens
+                                  document.execCommand('copy') ? res() : rej();
+                                  textArea.remove();
+                                  alert("Data copied to clipboard");
+                              });
                           }}>Copy results to clipboard</Button>
                         </Card.Body>
                       </Card>
